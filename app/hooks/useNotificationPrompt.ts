14bb0ec9668e2db982cb29854b3b195ee4a51b73
gabita30 <paracata999@proton.me>
2026-07-02 10:41:12 +0100

    Update useNotificationPrompt.ts

--- app/hooks/useNotificationPrompt.ts
+++ app/hooks/useNotificationPrompt.ts
@@ -4,7 +4,7 @@ import { supabase } from '@/lib/supabase'
 
 const VAPID_PUBLIC_KEY = 'BPCFgOOG0kXxVLxxh3jo-OYSHJ-w7Xdlr7gecdnavX1aJ6cepUdhVHfTQIHvB1KlJltUT7VH-i0yPxathvGeJgM'
 
-function urlBase64ToUint8Array(base64String: string): Uint8Array {
+function urlBase64ToUint8Array(base64String: string): BufferSource {
   const padding = '='.repeat((4 - (base64String.length % 4)) % 4)
   const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/')
   const rawData = window.atob(base64)
@@ -12,7 +12,7 @@ function urlBase64ToUint8Array(base64String: string): Uint8Array {
   for (let i = 0; i < rawData.length; i++) {
     outputArray[i] = rawData.charCodeAt(i)
   }
-  return outputArray
+  return outputArray.buffer as ArrayBuffer
 }
 
 export function useNotificationPrompt() {
